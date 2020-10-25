65ef79c6f586f99094e73a8192237e182898274d
Hans <lucas.hans@sap.com>
2020-10-25 03:49:58 -0300

    rebase

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { LinearProgress } from '@material-ui/core/';
 import { withStyles } from '@material-ui/core/styles'
-import { Data } from '../../interfaces/Data';
+import YellowBin from '../../images/trashcan yellow.svg'
+import GreenBin from '../../images/trashcan green.svg'
+import RedBin from '../../images/trashcan red.svg'
 import './Statistics.css'
 
 export const Statistics: React.FC = () => {
@@ -20,6 +22,11 @@ export const Statistics: React.FC = () => {
 
     const volunteers = currentData.filter((data: any) => data.assignee != '').length;
 
+    const inProgress = currentData.filter((data: any) => data.progress == "in-progress").length;
+    
+    const completed = currentData.filter((data: any) => data.progress == "completed").length;
+
+
     const BorderLinearProgress = withStyles((theme) => ({
         root: {
             height: 15,
@@ -30,7 +37,7 @@ export const Statistics: React.FC = () => {
         },
         bar: {
             borderRadius: 5,
-            backgroundColor: '#1a90ff',
+            backgroundColor: '#33261d',
         },
     }))(LinearProgress);
 
@@ -39,23 +46,72 @@ export const Statistics: React.FC = () => {
         <>
             <div className="statistics">
                 <h1 className= "h1">
-                    Reports  {totalReports}
+                     Reports - {totalReports}
                 </h1>
                 <h1 className="h1">
-                    Volunteers  {volunteers}
+                     Volunteers - {volunteers}
                 </h1>
-                <h1 className="h1">
-                    Reports Solutioned
-                </h1>
-                <div className="progress">
-                    <BorderLinearProgress
-                        value={(100 * volunteers / totalReports * 0.5)}
-                        variant='determinate'
-                    />
+                
+                <div className="binned" >
+                    <img className="bin" src={RedBin} />
+
+                    <h1 className="h1">
+                        New Reports - {totalReports - inProgress - completed}
+                    </h1>
+
                 </div>
+
+                <div className="binned" >
+                    <img className="bin" src={YellowBin} />
+                    
+                    <h1 className="h1">
+                        In Progress - {inProgress}
+                    </h1>
+
+                </div>
+
+                <div className="binned" >
+                    <img className="bin" src={GreenBin} />
+
+                    <h1 className="h1">
+                        Completed  - {completed}
+                    </h1>
+
+                </div>
+
+                <h1 className="text_right">By working together, we can all contribute to keep our cities clean and environmentally friendly. </h1>
+
             </div>
         </>
 
     );
 
 }
+
+
+/*
+                <div className="progress">
+                    <BorderLinearProgress
+                        value={(100 * inProgress / totalReports)}
+                        variant='determinate'
+                    />
+                </div>
+
+
+                <div className="binned" >
+                    <img className="bin" src={GreenBin} />
+
+                    <h1 className="h1">
+            Reports Completed
+                    </h1>
+                    
+
+                </div>
+                <div className="progress">
+                    <BorderLinearProgress
+                        value={(100 * completed / totalReports)}
+                        variant='determinate'
+                    />
+                </div>
+
+"*/
